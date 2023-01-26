b418d85731b4c42a91570ef737c0fca6219b5a60
mDavid <david.michael15.md@gmail.com>
2023-01-26 16:30:53 +0800

    Applied firebase to register page

--- src/firebase/initializeFirebase.js
+++ src/firebase/initializeFirebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getStorage } from "firebase/storage"
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -11,9 +11,11 @@ const firebaseConfig = {
   projectId: "chatkel",
   storageBucket: "chatkel.appspot.com",
   messagingSenderId: "543143986526",
-  appId: "1:543143986526:web:292e8e8dc3c4932756bbcf"
-};
+  appId: "1:543143986526:web:292e8e8dc3c4932756bbcf",
+}
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
+export const app = initializeApp(firebaseConfig)
+export const auth = getAuth()
+export const storage = getStorage()
+export const db = getFirestore()
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,35 +1,84 @@
-import React from "react"
+import React, { useState } from "react"
 import { BiImageAdd } from "react-icons/bi"
 import "./register.scss"
 
+//firebase
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
+import { auth, storage, db } from "../../firebase/initializeFirebase"
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"
+import { doc, setDoc } from "firebase/firestore"
+
 const Register = () => {
+  const [err, setErr] = useState({ isError: false, message: "" })
+
+  const handleRegister = async (e) => {
+    e.preventDefault()
+    const displayName = e.target[0].value
+    const email = e.target[1].value
+    const password = e.target[2].value
+    const file = e.target[3].files[0]
+
+    try {
+      //create firebase auth sign in
+      const rspns = await createUserWithEmailAndPassword(auth, email, password)
+
+      //upload image
+      const storageRef = ref(storage, displayName)
+
+      const uploadTask = uploadBytesResumable(storageRef, file)
+
+      await uploadTask.then(() => {
+        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+          await updateProfile(rspns.user, {
+            displayName,
+            photoURL: downloadURL,
+          })
+
+          //add data firestore for new user
+          await setDoc(doc(db, "users", rspns.user.uid), {
+            uid: rspns.user.uid,
+            displayName,
+            email,
+            photoURL: downloadURL,
+          })
+        })
+      })
+    } catch (error) {
+      setErr({ ...err, isError: true, message: error.message })
+    }
+  }
+
   return (
     <div className="register">
       <div className="wrapper">
         <div className="title">
           <h1>Register to Chat-kel</h1>
         </div>
-        <form>
+        <form
+          onSubmit={(e) => {
+            handleRegister(e)
+          }}
+        >
           <div>
-            <label for="name">Name</label>
+            <label htmlFor="name">Name</label>
             <input
               id="name"
               type="text"
               placeholder="Enter your name"
               required
             />
-            <label for="email">Email</label>
+            <label htmlFor="email">Email</label>
             <input
               id="email"
               type="email"
               placeholder="Enter your email"
               required
             />
-            <label for="pass">Password</label>
+            <label htmlFor="pass">Password</label>
             <input
               id="pass"
               type="password"
-              placeholder="Enter Password"
+              placeholder="Enter password"
               required
             />
             <input type="file" id="file" style={{ display: "none" }} />
@@ -40,6 +89,7 @@ const Register = () => {
           </label>
           <button className="button-primary"> Sign Up</button>
         </form>
+        {err.isError && <span>{err.message}</span>}
         <span>
           Already have an account? <a href="/login">Login</a>
         </span>
